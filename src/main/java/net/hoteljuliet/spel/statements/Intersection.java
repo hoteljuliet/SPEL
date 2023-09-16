b3c613410275629cfddfcacc4e513e7e1d4966d9
Dennis Colasurdo <denniscolasurdo@Denniss-MBP.lan>
2023-09-15 22:22:04 -0400

    initial checkin

---
+++ src/main/java/net/hoteljuliet/spel/statements/Intersection.java
@@ -0,0 +1,9 @@
+package net.hoteljuliet.spel.statements;
+
+import net.hoteljuliet.spel.Step;
+
+// TODO: Given the path to 2 collections, compute the intersection and output to dest
+@Step(tag = "intersection")
+public class Intersection {
+    ;
+}
