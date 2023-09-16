0d49b323a63e71da5326dedcb5416c2bb26c6903
Dennis Colasurdo <denniscolasurdo@Denniss-MBP.lan>
2023-09-15 22:26:44 -0400

    initial checkin

--- src/main/java/net/hoteljuliet/spel/statements/Flatten.java
+++ src/main/java/net/hoteljuliet/spel/statements/Flatten.java
@@ -1,8 +1,11 @@
 package net.hoteljuliet.spel.statements;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import net.hoteljuliet.spel.Context;
 import net.hoteljuliet.spel.Step;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -10,9 +13,33 @@ import java.util.Optional;
 @Step(tag = "flatten")
 public class Flatten extends StatementBaseStep {
 
+    private String source;
+    private String root;
+    private String dest;
+
+    @JsonCreator
+    public Flatten(@JsonProperty(value = "source", required = true) String source,
+                   @JsonProperty(value = "root", required = true) String root,
+                   @JsonProperty(value = "dest", required = true) String dest) {
+        super();
+        this.source = source;
+        this.root = root;
+        this.dest = dest;
+    }
+
     @Override
     public Optional<Boolean> doExecute(Context context) throws Exception {
-        return Optional.empty();
+
+        if (context.containsKey(source)) {
+            Map<String, Object> original = context.getField(source);
+            Map<String, Object> flattened = new HashMap<>();
+            flatten(root, original, flattened);
+            context.addField(dest, flattened);
+        }
+        else {
+            missingField.increment();
+        }
+        return NEITHER;
     }
 
     public void flatten(String key, Map<String, Object> in, Map<String, Object> out) {
