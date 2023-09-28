69fb2049273b2a55ba537b3c845c7732bb0b7911
Dennis Colasurdo <denniscolasurdo@Denniss-MacBook-Pro.local>
2023-09-28 13:16:47 -0400

    initial checkin

--- src/test/java/io/github/hoteljuliet/spel/predicates/HasFieldTest.java
+++ src/test/java/io/github/hoteljuliet/spel/predicates/HasFieldTest.java
@@ -1,7 +1,6 @@
 package io.github.hoteljuliet.spel.predicates;
 
 import io.github.hoteljuliet.spel.Context;
-import io.github.hoteljuliet.spel.mustache.TemplateLiteral;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -13,38 +12,30 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(Parameterized.class)
-public class InTest {
+public class HasFieldTest {
     private final Map<String, Object> contextValues;
-    private final TemplateLiteral value;
-    private final TemplateLiteral list;
+    private final String value;
     private final Boolean expected;
 
     @Parameterized.Parameters()
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][] {
-
-                // literal in template
-                {Context.mapOf("list", Arrays.asList(1, 2, 3)), 1, "{{list}}", true},
-
-                // template in literal
-                {Context.mapOf("value", 1), "{{value}}", Arrays.asList(1, 2, 3), true},
-
-                // template in template
-               {Context.mapOf("value", 1, "list", Arrays.asList(1, 2, 3)), "{{value}}", "{{list}}", true}
+                {Context.mapOf("field1", "value1"), "field1", true},
+                {Context.mapOf("field1", "value1"), "field2", false},
         });
     }
-    public InTest(Map<String, Object> contextValues, Object value, Object list, Boolean expected) {
+
+    public HasFieldTest(Map<String, Object> contextValues, String value, Boolean expected) {
         this.contextValues = contextValues;
-        this.list = new TemplateLiteral(list);
-        this.value = new TemplateLiteral(value);
+        this.value = value;
         this.expected = expected;
     }
 
     @Test
     public void test() throws Exception {
         Context context = new Context(contextValues);
-        In in = new In(value, list);
-        Optional<Boolean> result = in.doExecute(context);
+        HasField hasField = new HasField(value);
+        Optional<Boolean> result = hasField.doExecute(context);
         assertThat(result.isPresent());
         assertThat(result.get()).isEqualTo(expected);
     }
